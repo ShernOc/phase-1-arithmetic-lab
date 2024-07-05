1b60f945e7ccf02f0c85cd7180628984ad9074e9
Sherlyne <sherlynea1994@hotmail.com>
2024-07-06 00:44:56 +0300

    Modified index.js file

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
-// Write your code here
+// Write your code here : Multiply 
+  let num1= 31;
+  let num2 = 2;
+  let multiply = num1 * num2;
+
+console.log(multiply); 
+
+//Random Integer
+let random=Number.isInteger(Math.random()*5)+1;
+console.log(random);
+
+// MOD
+
+let num3 = 16; 
+let num4 = 6; 
+let mod = num3 % num4 
+
+console.log(mod); 
+
+//MAX
+let max= Math.max(10,15,20);
+console.log(max)
+
+
+
+
